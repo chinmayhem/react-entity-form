1aaf2b4a5c1ba7cb41029e6131f2bdf0299ca2f0
chicho17 <chaudharychinmay@gmail.com>
2020-08-21 17:01:22 +0530

    Updates

---
+++ precache-manifest.37ff53064b5710d8d35338d03abf2572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4db3a85e57deee0aa62eecf1c48db7f",
+    "url": "/react-entity-form/index.html"
+  },
+  {
+    "revision": "9ac27b39697fde6ff822",
+    "url": "/react-entity-form/static/css/main.13445d9c.chunk.css"
+  },
+  {
+    "revision": "5a974b41247f6167f60e",
+    "url": "/react-entity-form/static/js/2.7154db44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-entity-form/static/js/2.7154db44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac27b39697fde6ff822",
+    "url": "/react-entity-form/static/js/main.f439dce5.chunk.js"
+  },
+  {
+    "revision": "2c7a755f1d0db9204179",
+    "url": "/react-entity-form/static/js/runtime-main.93826aab.js"
+  }
+]);
